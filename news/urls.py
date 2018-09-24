5c38950bae8b60ea427a06c8fbd5738632ed4c46
Sam Oluoch <samsoluoch@gmail.com>
2018-09-24 15:33:41 +0300

    added functionality for fetching archived news using date search

--- news/urls.py
+++ news/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.news_of_day,name='newsToday')
+    url('^today/$',views.news_of_day,name='newsToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews')
 ]
 '''
 In the above, we create a new URLpattern that will match the URL today and connect it to our view function news_of_day. 
