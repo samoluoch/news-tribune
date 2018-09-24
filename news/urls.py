2706a422806c1e33f30e463101cd6aff0f766104
Sam Oluoch <samsoluoch@gmail.com>
2018-09-24 12:56:52 +0300

    added time functionality to display news times

--- news/urls.py
+++ news/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
+    url('^today/$',views.news_of_day,name='newsToday')
 ]
+'''
+In the above, we create a new URLpattern that will match the URL today and connect it to our view function news_of_day. 
+'''
+
 
--- news/views.py
+++ news/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import datetime as dt
+
 
 # Create your views here.
 
 def welcome(request):
     return HttpResponse('Welcome to the Moringa Tribune')
+
+def news_of_day(request):
+    '''
+    Function that returns the current date
+    '''
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
