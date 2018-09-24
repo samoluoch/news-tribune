1fdd72e64e17e1c272078629c0005662758e9053
Sam Oluoch <samsoluoch@gmail.com>
2018-09-24 14:06:49 +0300

    created a function for displaying the day of the week alongside the date on every news article

--- news/views.py
+++ news/views.py
@@ -10,15 +10,29 @@ def welcome(request):
     return HttpResponse('Welcome to the Moringa Tribune')
 
 def news_of_day(request):
-    '''
-    Function that returns the current date
-    '''
     date = dt.date.today()
+
+    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
     html = f'''
         <html>
             <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
             </body>
         </html>
             '''
     return HttpResponse(html)
+
+
+def convert_dates(dates):
+    
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
+
+
