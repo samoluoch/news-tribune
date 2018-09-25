ec72ef75d330794ffb2b43676f33d1947917548c
Sam Oluoch <samsoluoch@gmail.com>
2018-09-25 14:53:44 +0300

    updated the templates to disply the day's news and past day's news

--- news/views.py
+++ news/views.py
@@ -20,11 +20,9 @@ def news_of_day(request):
 
 # View Function to present news from past days
 def past_days_news(request, past_date):
-
     try:
         # Converts data from the string Url
         date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
-
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
@@ -33,4 +31,5 @@ def past_days_news(request, past_date):
     if date == dt.date.today():
         return redirect(news_of_day)
 
-    return render(request, 'all-news/past-news.html', {"date": date})
+    news = Article.days_news(date)
+    return render(request, 'all-news/past-news.html', {"date": date, "news": news})
